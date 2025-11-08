05b32f72a19046536b83e852fda57acf5b098295
lourina-karkoa <nanasy893@gmail.com>
2025-11-09 00:31:44 +0300

    pages2

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  base: '/Store/',
   plugins: [react()],
+  base: '/Store/', // 👈 مهم جداً عند النشر على GitHub Pages
 })
